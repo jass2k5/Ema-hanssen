4171a96e32290051948c9012ec3c02b921a2c70a
jass2k5 <jsssndl@gmail.com>
2026-03-16 17:08:19 +0530

    dynamic and clickable navigartion buttons added

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ const scrollpictures = [
     {src:'img/workscroll1.avif',alt:'hero-image'},
     {src: 'img/img3.webp',alt: 'hero-image'}
 ];
+let injecting = "";
 scrollpictures.forEach((pics,idx) =>{
     let showfirstclass = "";
     if(idx === 0){
@@ -34,14 +35,23 @@ scrollpictures.forEach((pics,idx) =>{
     }else if(idx === 1){
         showfirstclass = "next";
     }
-    const injecting = `<img class="hero-image ${showfirstclass}" src="${pics.src}" alt="${pics.alt}">`;
-    scrollimages.innerHTML += injecting;
+    injecting += `<img class="hero-image ${showfirstclass}" src="${pics.src}" alt="${pics.alt}">`;
 });
+scrollimages.innerHTML = injecting;
+
+let indicatorcontainer = document.querySelector('.imageindicators');
+let holder = "";
+for(let i = 0;i<scrollpictures.length;i++){
+    holder += ` <i class="ri-checkbox-blank-circle-fill"></i>`;
+}
+indicatorcontainer.innerHTML = holder;
+let currentindicator = 0;
 let autoslider;
 let currentindex = 0 ;
 const intervaltime = 4000;
+let images = document.querySelectorAll('.hero-image');
 function slidingpanel(){
-        let images = document.querySelectorAll('.hero-image');
+        let inds = document.querySelectorAll(".ri-checkbox-blank-circle-fill");
         if(images.length === 0){
             return;
         }
@@ -50,14 +60,33 @@ function slidingpanel(){
         });
         images[currentindex].classList.add('slideout');
         images[currentindex].classList.remove('active');
+        inds[currentindicator].classList.remove('whitedot');
 
         currentindex = (currentindex + 1) % images.length;
+        currentindicator = currentindex;
         images[currentindex].classList.add('active');
+        inds[currentindicator].classList.add('whitedot');
+
 
         let nextimage = (currentindex + 1)%images.length;
         images[nextimage].classList.add('next');
 }
 autoslider = setInterval(slidingpanel,intervaltime);
+const inds = document.querySelectorAll(".ri-checkbox-blank-circle-fill");
+inds.forEach((dot,idx)=>{
+    dot.addEventListener('click',()=>{
+        clearInterval(autoslider);
+        images.forEach(img=>{
+            img.classList.remove('active','slideout','next');
+        });
+        inds[currentindicator].classList.remove('whitedot');
+        currentindex = idx;
+        currentindicator = currentindex;
+        images[currentindex].classList.add('active');
+        inds[currentindicator].classList.add('whitedot');
+        autoslider = setInterval(slidingpanel,intervaltime);
+    })
+})
 let slidersection = document.querySelector('.duo-1')
 let container = document.querySelector('.container');
 const projectImages = [
