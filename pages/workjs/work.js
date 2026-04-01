5d0995d84860269db9f16798984d4405762a2a17
jass2k5 <jsssndl@gmail.com>
2026-04-01 15:14:25 +0530

    completed array of object for work sec

---
+++ pages/workjs/work.js
@@ -0,0 +1,86 @@
+export const workProjectsData = [
+    {
+        h3: "Beige",
+        span: "Commercial 2024",
+        src: "../img/img1.webp",
+        alt: "Beige project"
+    },
+    {
+        h3: "Darkness",
+        span: "Commercial 2024",
+        src: "../img/img2.webp",
+        alt: "Darkness project"
+    },
+    {
+        h3: "Gentlemen",
+        span: "Editorial 2024",
+        src: "../img/img3.webp",
+        alt: "Gentlemen project"
+    },
+    {
+        h3: "City",
+        span: "Commercial 2024",
+        src: "../img/img4.webp",
+        alt: "City project"
+    },
+    {
+        h3: "Nature",
+        span: "Editorial 2024",
+        src: "../img/img5.webp",
+        alt: "Nature project"
+    },
+    {
+        h3: "Brand",
+        span: "Editorial 2023",
+        src: "../img/image.png",
+        alt: "Brand project"
+    },
+    {
+        h3: "Studio",
+        span: "Commercial 2023",
+        src: "../img/img7.webp",
+        alt: "Studio project"
+    },
+    {
+        h3: "Colors",
+        span: "Editorial 2023",
+        src: "../img/img8.webp",
+        alt: "Colors project"
+    },
+    {
+        h3: "Duo",
+        span: "Commercial 2023",
+        src: "../img/img9.webp",
+        alt: "Duo project"
+    },
+    {
+        h3: "Elegance",
+        span: "Editorial 2023",
+        src: "../img/img10.avif",
+        alt: "Elegance project"
+    },
+    {
+        h3: "Hat",
+        span: "Commercial 2023",
+        src: "../img/img11.png",
+        alt: "Hat project"
+    },
+    {
+        h3: "Black & White",
+        span: "Editorial 2023",
+        src: "../img/img12.png",
+        alt: "Black and White project"
+    },
+    {
+        h3: "Streets",
+        span: "Commercial 2023",
+        src: "../img/img13.png",
+        alt: "Streets project"
+    },
+    {
+        h3: "Stoic",
+        span: "Commercial 2023",
+        src: "../img/img14.png",
+        alt: "Stoic project"
+    }
+];
